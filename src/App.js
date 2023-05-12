8064ebf5fe1f6a2a314e612d35e8219656bddf72
ismail-29 <esmailpasha200@gmail.com>
2023-05-12 11:29:52 +0530

    project 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {marked} from 'marked';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
+function MarkdownPreviewer() {
+  const [text, setText] = useState(`# H1
+    ## H2
+    [title](https://www.example.com)
+    \`code\`
+    \`\`\`
+{
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+}
+\`\`\`
+- First item
+- Second item
+- Third item
+
+> blockquote
+
+**bold text**
+![alt text](image.jpg)
+  `);
+
+  const handleInputChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const getMarkdownText = () => {
+    return { __html: marked(text) };
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="markdown-previewer-container">
+      <h1>Markdown Previewer</h1>
+      <div className="editor-container">
+        <label htmlFor="editor">Editor</label>
+        <br />
+        <textarea
+          id="editor"
+          value={text}
+          onChange={handleInputChange}
+        ></textarea>
+      </div>
+      <div className="preview-container">
+        <label htmlFor="preview">Preview</label>
+        <br />
+        <div id="preview" dangerouslySetInnerHTML={getMarkdownText()}></div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default MarkdownPreviewer;
